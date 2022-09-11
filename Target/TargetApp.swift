fbba6038d1747dfcc97821e380ef0c65b0728ad1
Joseph DeWeese <rangerboy901@icloud.com>
2022-09-11 15:50:10 -0500

    Initial Commit

---
+++ Target/TargetApp.swift
@@ -0,0 +1,17 @@
+//
+//  TargetApp.swift
+//  Target
+//
+//  Created by Joseph Wil;liam DeWeese on 9/11/22.
+//
+
+import SwiftUI
+
+@main
+struct TargetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
